1ca99553d8c1775a142f57f9a3f92104cd477cfa
omarnama <57706936+omarnama@users.noreply.github.com>
2020-03-02 10:49:28 +0100

    fix

--- src/main/java/it/jac/javadb/util/Utils.java
+++ src/main/java/it/jac/javadb/util/Utils.java
@@ -2,7 +2,8 @@ package it.jac.javadb.util;
 
 
 import java.text.SimpleDateFormat;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -10,9 +11,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import it.jac.javadb.entity.Persona;
+import it.jac.javadb.dao.MalattiaDao;
+import it.jac.javadb.entity.Malattia;
 import it.jac.javadb.dao.PersonaDao;
 
-public class Utils {
+public class Utils implements Iterator<Persona> {
 	
 	private static final Logger log = LogManager.getLogger(Utils.class);
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -56,6 +59,52 @@ public class Utils {
 		
 		
 }
+	
+	public static int position = -1;
+	public static List<Persona> persone = new ArrayList<Persona>();
+	
+	public void stampaListaPersone(List<Persona> list)
+	{
+		PersonaDao dao = new PersonaDao();
+		list = dao.findAll();
+		System.out.println(list.get(0));
+		for(int i = 0; i < list.size() - 1; i++)
+		{
+			System.out.println(next());
+		}
+	}
+	
+	public void stampaListaMalattie(List<it.jac.javadb.entity.Malattia> list)
+	{
+		it.jac.javadb.dao.MalattiaDao dao = new it.jac.javadb.dao.MalattiaDao();
+		list = dao.findAll();
+		System.out.println(list.get(0));
+		for(int i = 0; i < list.size() - 1; i++)
+		{
+			System.out.println(next());
+		}
+	}
+	
+
+	
+	@Override
+	public boolean hasNext() {
+		return next() != null;
+	}
+	
+	@Override
+	public Persona next() {
+		position++;
+        return position < persone.size() ? persone.get(position) : null;
+	}
+	
+	@Override
+	public void remove() 
+	{
+		Iterator.super.remove();
+		persone.remove(persone.get(position));
+	}
+	
 }
 
 /*
